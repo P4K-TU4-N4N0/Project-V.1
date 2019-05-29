9cbcc403fc698aa85886718b563234c9c4a64b74
P4K-TU4-N4N0 <51157995+P4K-TU4-N4N0@users.noreply.github.com>
2019-05-29 18:19:45 +0700

    Add files via upload

---
+++ TU4.sh
@@ -0,0 +1,247 @@
+#!/bin/sh
+#Mau Ngapain Goblok
+#Di Recode Gak Bakalan Jadiin lu Mastah
+#recode goblok
+#ganti author?
+#masih ada kah?
+#hahahahahahah
+#GOBLOK
+
+
+Cy='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+e='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #putih
+ku='\033[33;1m' #kuning
+
+
+
+clear
+figlet -f slant "wait" | lolcat
+clear
+
+
+figlet -f slant "username" | lolcat
+read  "username" wow
+figlet -f slant "pasword" | lolcat
+read  "pasword" now
+
+
+[ $wow = P4K N4N0 ]
+[ $now = rahasia ]
+
+clear
+
+echo $me "***************************************"
+echo $me "***************************************"
+echo $me "***************************************"
+echo $me "***************************************"
+echo $me "***************************************"
+echo $me "***************************************"
+echo $pu "***************************************"
+echo $pu "***************************************"
+echo $pu "***************************************"
+echo $pu "***************************************"
+echo $pu "***************************************"
+echo $pu "***************************************"
+
+
+echo "===============================================" | lolcat
+echo "             author by P4K TU4 N4N0            " | lolcat
+echo "               C L 4 Y HACKER T34M             " | lolcat
+echo "            NO SYSTEM IS SAFE MY FRIENDS       " | lolcat
+echo "               MASTAH IS A ILUSION             " | lolcat
+echo "           TOOLS MASIH DALAM PERKEMBANGAN      " | lolcat
+echo "===============================================" | lolcat
+
+
+
+
+echo $me "install" $pu "install bahan bahannya dulu gan"
+echo $me "00" $pu "keluar dari tools"
+echo $me "01" $pu "lacak orang dengan web palsu"
+echo $me "02" $pu "cekrek orang pakek web palsu"
+echo $me "03" $pu "liat peta di termux"
+echo $me "04" $pu "main api apian di termux biar anget"
+echo $me "05" $pu "liat aja sendiri biar tau"
+echo $me "06" $pu "buat script deface"
+echo $me "07" $pu "deface website"
+echo $me "08" $pu "phising all sosmed"
+echo $me "09" $pu "brutal sms"
+echo $me "10" $pu "trojan"
+echo $me "11" $pu "hamerring ddos"
+echo $me "12" $pu "scan-ip"
+echo $me "13" $pu "hack cctv"
+
+
+echo "╭─P4K-TU4-N4N0[pilih angka]" | lolcat
+read -p "╰─Angka?" pil
+
+if [ pil = install ]
+then
+clear
+pkg install unzip
+pkg install php                                                                               pkg install nodejs
+apt update && apt upgrade                                                                     apt install python2
+pip2 install urllib3 chardet certifi idna requests
+pkg install git                                                                               pip install requests
+pip2 install requests
+pip install mechanize
+pkg install unstable-repo
+pip2 install mechanize
+pkg install curl
+pkg install openssh
+pkg install php
+pkg install python
+pkg install ruby
+pkg install gem
+gem install lolcat
+pkg install git
+pkg install php
+pkg install ruby cowsay toilet figlet
+pkg install neofetch
+pkg install nano
+pkg install python
+pkg install python2
+pkg install python3
+figlet -f slant " S U K S E S "|lolcat
+fi
+
+
+
+if [ $pil = 00 ]
+then
+clear
+figlet -f slant "keluar" | lolcat
+sleep 2
+echo $me "terimaksih telah menggunakan tools saya"
+sleep 2
+echo $pu "jangan lula folow ig saya RifkyXW69"
+sleep 2
+echo $me "terimaksih yang telah mendukung saya"
+exit
+fi
+
+if [ $pil = 01 ]
+then
+clear
+figlet -f slant "tunggu" | lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/locator
+cd locator
+bash locator.sh
+fi
+
+if [ $pil = 02 ]
+then
+clear
+figlet -f slant "tunggu" | lolcat
+sleep 1
+git clone https://github.com/thelinuxchoice/saycheese
+cd saycheese
+bash saycheese.sh
+fi
+
+if [ $pil = 03 ]
+then
+clear
+figlet -f slant "tunggu" | lolcat
+sleep 1
+telnet mapscii.me
+fi
+
+if [ $pil = 04 ]
+then
+clear
+figlet -f slant "tunggu" | lolcat
+sleep 1
+apt install libcaca
+cacafire
+fi
+
+if [ $pil = 05 ]
+then
+clear
+figlet -f slant "tunggu" | lolcat
+sleep 1
+apt install libcaca
+cacademo
+fi
+
+if [ $pil = 06 ]
+then
+clear
+figlet -f slant "tunggu" | lolcat
+sleep 1
+git clone https://github.com/404rgr/webdav77
+cd webdav77
+chmod +x *
+./install.sh
+bash webdav77.sh
+fi
+
+if [ $pil = 07 ]
+then
+clear
+git clone https://github.com/Amriez/AOCDEFACE
+cd AOCDEFACE
+chmod +x *
+./install.sh
+clear
+sh AOC.sh
+fi
+
+if [ $pil = 08 ]
+then
+clear
+git clone https://github.com/TheLinuxChoice/shellphish
+cd shellphish
+bash shellphish.sh
+fi
+
+if [ $pil = 09 ]
+then
+clear
+git clone https://github.com/N74NK/brutalsms
+cd brutalsms
+python2 brutalsms.py
+fi
+
+if [ $pil = 10 ]
+then
+clear
+chmod 777 Trojan.py
+python3 Trojan.py
+fi
+
+if [ $pil = 11 ]
+then
+clear
+git clone https://github.com/cyweb/hammer
+cd hammer
+chmod 777 hammer.py
+python3 hammer.py
+fi
+
+if [ $pil = 12 ]
+then
+clear
+echo "=====================================" | lolcat
+echo "        author by P4K TU4 N4N0       " | lolcat
+echo "          C L 4 Y HACKER T34M        " | lolcat
+echo "=====================================" | lolcat
+
+echo "[!] gunkan vpn cuk biar aman sedikit" | lolcat
+echo "[√] masukan website target (www.target.com) : " $target | lolcat
+
+read target
+ping $target
+fi
+
+if [ $pil = 13 ]
+then
+clear
+python2 scan.py
+fi
